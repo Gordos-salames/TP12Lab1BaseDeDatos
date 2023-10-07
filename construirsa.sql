aad2fe87f0149f2249d2315360b73f2f46b5775a
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-07 15:32:57 -0300

    Add files via upload

--- construirsa.sql
+++ construirsa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-10-2023 a las 00:53:09
+-- Tiempo de generación: 07-10-2023 a las 20:30:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
