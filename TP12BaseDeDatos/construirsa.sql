52a8dcb0982766bec9cbe72c5f1d21b6520f7c2c
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-10-07 15:32:32 -0300

    Add files via upload

---
+++ TP12BaseDeDatos/construirsa.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-10-2023 a las 20:30:32
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `construirsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `idEmpleado` int(11) NOT NULL,
+  `dni` bigint(20) NOT NULL,
+  `apellido` varchar(58) NOT NULL,
+  `nombre` varchar(58) NOT NULL,
+  `acceso` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`idEmpleado`, `dni`, `apellido`, `nombre`, `acceso`, `activo`) VALUES
+(1, 48526351, 'Rotta', 'Alejandro', 0, 0),
+(2, 42258623, 'Cuk', 'Gonzalo', 0, 0),
+(3, 45285201, 'Mata', 'Laurencio', 0, 1),
+(4, 4223978, 'Veliz', 'Ivo', 1, 1),
+(5, 42239915, 'Veliz', 'Sinai', 1, 1),
+(6, 45850290, 'Rotta', 'Francisco', 1, 1),
+(7, 45750239, 'Cuk', 'Esteban', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `herramienta`
+--
+
+CREATE TABLE `herramienta` (
+  `idHerramienta` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `herramienta`
+--
+
+INSERT INTO `herramienta` (`idHerramienta`, `nombre`, `descripcion`, `stock`, `estado`) VALUES
+(1, 'Destornillador', 'Punta Plana', 10, 1),
+(2, 'Martillo', 'Maza', 3, 1),
+(3, 'Amoladora', 'Cortar,Pulir,Desbastar', 2, 1),
+(4, 'Taladro', 'De Mano', 5, 1),
+(5, 'Serrucho', 'De Corte', 8, 0),
+(6, 'Destornillador', 'Philips', 11, 1),
+(7, 'Sierra', 'Metal', 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+CREATE TABLE `movimiento` (
+  `idMovimiento` int(11) NOT NULL,
+  `idEmpleado` int(11) NOT NULL,
+  `idHerramienta` int(11) NOT NULL,
+  `fechap` date NOT NULL,
+  `fechad` date NOT NULL,
+  `cantidadret` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `movimiento`
+--
+
+INSERT INTO `movimiento` (`idMovimiento`, `idEmpleado`, `idHerramienta`, `fechap`, `fechad`, `cantidadret`) VALUES
+(4, 2, 2, '2023-12-02', '2023-11-05', 5),
+(5, 3, 3, '2023-08-15', '2023-09-16', 10),
+(7, 1, 1, '2023-02-17', '2023-03-15', 8);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`idEmpleado`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `herramienta`
+--
+ALTER TABLE `herramienta`
+  ADD PRIMARY KEY (`idHerramienta`);
+
+--
+-- Indices de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD PRIMARY KEY (`idMovimiento`),
+  ADD KEY `idEmpleado` (`idEmpleado`),
+  ADD KEY `idHerramienta` (`idHerramienta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `idEmpleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `herramienta`
+--
+ALTER TABLE `herramienta`
+  MODIFY `idHerramienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  MODIFY `idMovimiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD CONSTRAINT `movimiento_ibfk_1` FOREIGN KEY (`idEmpleado`) REFERENCES `empleado` (`idEmpleado`),
+  ADD CONSTRAINT `movimiento_ibfk_2` FOREIGN KEY (`idHerramienta`) REFERENCES `herramienta` (`idHerramienta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ TP12BaseDeDatos/src/tp12basededatos/TP12BaseDeDatos.java
@@ -0,0 +1,179 @@
+package tp12basededatos;
+
+import java.awt.HeadlessException;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+public class TP12BaseDeDatos {
+    public static void main(String[] args) {
+  /*try {
+            // Cargar el Driver de conexión
+            Class.forName("org.mariadb.jdbc.Driver");
+            // Establecer la conexión a la base de datos
+            String URL = "jdbc:mariadb://localhost:3306/construirsa";
+            String usuario = "root";
+            String password = "";
+            Connection con=DriverManager.getConnection(URL,usuario,password);
+            //Agregar un empleado
+            String sql="INSERT INTO empleado (dni,apellido,nombre,acceso,activo)"
+                    + "Values (45750239, 'Cuk', 'Esteban',1,1)";
+            PreparedStatement ps=con.prepareStatement(sql);
+            int filas = ps.executeUpdate();
+            if(filas > 0){
+                JOptionPane.showMessageDialog(null, "Empleado Agregado Exitosamente");
+            }
+            System.out.println("Exito");
+        } catch (ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Error al cargar al driver");
+        } catch (SQLException ex){
+           int error= ex.getErrorCode();
+           if(error==1146){
+               JOptionPane.showMessageDialog(null, "Tabla Inexistente");
+           } else if(error==1049){
+               JOptionPane.showMessageDialog(null, "BD inexistente");
+           } else{
+               JOptionPane.showMessageDialog(null, "Error SQL");
+           }
+        }
+  */
+    /*try {
+      // Cargar el Driver de conexión
+            Class.forName("org.mariadb.jdbc.Driver");
+            // Establecer la conexión a la base de datos
+            String URL = "jdbc:mariadb://localhost:3306/construirsa";
+            String usuario = "root";
+            String password = "";
+            Connection con=DriverManager.getConnection(URL,usuario,password);
+            //Agregar una herramienta
+            String sql="INSERT INTO herramienta (nombre,descripcion,stock,estado)"
+                    + "Values ('Sierra', 'Metal',6,1)";
+            PreparedStatement ps=con.prepareStatement(sql);
+            int filas = ps.executeUpdate();
+            if(filas > 0){
+                JOptionPane.showMessageDialog(null, "Herramienta Agregada Exitosamente");
+            }
+            System.out.println("Exito");
+        } catch (ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Error al cargar al driver");
+        } catch (SQLException ex){
+           int error= ex.getErrorCode();
+           if(error==1146){
+               JOptionPane.showMessageDialog(null, "Tabla Inexistente");
+           } else if(error==1049){
+               JOptionPane.showMessageDialog(null, "BD inexistente");
+           } else{
+               JOptionPane.showMessageDialog(null, "Error SQL");
+           }
+        }*/
+        /*try {
+                // Cargar el Driver de conexión
+            Class.forName("org.mariadb.jdbc.Driver");
+            // Establecer la conexión a la base de datos
+            String URL = "jdbc:mariadb://localhost:3306/construirsa";
+            String usuario = "root";
+            String password = "";
+            Connection con=DriverManager.getConnection(URL,usuario,password);
+            //Consultar el estado de las herramientas con stock mayor a 10 e informacion de si estan activas y en prestamos
+                      String consultaSQL = "SELECT h.nombre AS nombre_herramienta, h.descripcion, h.stock, h.estado, " +
+                "m.idEmpleado, e.nombre AS nombre_empleado, e.apellido, m.fechap, m.fechad " +
+                "FROM herramienta h " +
+                "LEFT JOIN movimiento m ON h.idHerramienta = m.idHerramienta " +
+                "LEFT JOIN empleado e ON m.idEmpleado = e.idEmpleado " +
+                "WHERE h.stock > 10";
+
+            PreparedStatement ps = con.prepareStatement(consultaSQL);
+            ResultSet resultado = ps.executeQuery();
+
+            // Procesar y mostrar los resultados
+            while (resultado.next()) {
+                String nombreHerramienta = resultado.getString("nombre_herramienta");
+                String descripcion = resultado.getString("descripcion");
+                int stock = resultado.getInt("stock");
+                boolean estado = resultado.getBoolean("estado");
+                int idEmpleado = resultado.getInt("idEmpleado");
+                String nombreEmpleado = resultado.getString("nombre_empleado");
+                String apellidoEmpleado = resultado.getString("apellido");
+                Date fechaPrestamo = resultado.getDate("fechap");
+                Date fechaDevolucion = resultado.getDate("fechad");
+
+                System.out.println("Nombre de la Herramienta: " + nombreHerramienta);
+                System.out.println("Descripción: " + descripcion);
+                System.out.println("Stock: " + stock);
+                System.out.println("Estado: " + (estado ? "Activo" : "Inactivo"));
+                System.out.println("ID del Empleado: " + idEmpleado);
+                System.out.println("Nombre del Empleado: " + nombreEmpleado + " " + apellidoEmpleado);
+                System.out.println("Fecha de Préstamo: " + fechaPrestamo);
+                System.out.println("Fecha de Devolución: " + fechaDevolucion);
+                System.out.println("-------------------");
+            }
+
+            resultado.close();
+            ps.close();
+        } catch (ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Error al cargar al driver");
+        } catch (SQLException ex){
+           int error= ex.getErrorCode();
+           if(error==1146){
+               JOptionPane.showMessageDialog(null, "Tabla Inexistente");
+           } else if(error==1049){
+               JOptionPane.showMessageDialog(null, "BD inexistente");
+           } else{
+               JOptionPane.showMessageDialog(null, "Error SQL");
+           }
+        }*/
+               /*try {
+                // Cargar el Driver de conexión
+            Class.forName("org.mariadb.jdbc.Driver");
+            // Establecer la conexión a la base de datos
+            String URL = "jdbc:mariadb://localhost:3306/construirsa";
+            String usuario = "root";
+            String password = "";
+            Connection con=DriverManager.getConnection(URL,usuario,password);
+                       // Consulta para encontrar el primer empleado ingresado (asumiendo que tiene el ID más bajo)
+            String consultaEmpleadoSQL = "SELECT idEmpleado FROM empleado ORDER BY idEmpleado ASC LIMIT 1";
+            PreparedStatement psEmpleado = con.prepareStatement(consultaEmpleadoSQL);
+            ResultSet resultadoEmpleado = psEmpleado.executeQuery();
+
+            if (resultadoEmpleado.next()) {
+                int idEmpleado = resultadoEmpleado.getInt("idEmpleado");
+
+                // Actualizar el estado del empleado a inactivo
+                String actualizarEstadoSQL = "UPDATE empleado SET activo = ? WHERE idEmpleado = ?";
+                PreparedStatement psActualizarEstado = con.prepareStatement(actualizarEstadoSQL);
+                psActualizarEstado.setBoolean(1, false); // Establece el estado a inactivo
+                psActualizarEstado.setInt(2, idEmpleado);
+                
+                int filasAfectadas = psActualizarEstado.executeUpdate();
+                if (filasAfectadas > 0) {
+                    System.out.println("Empleado con ID " + idEmpleado + " dado de baja exitosamente.");
+                } else {
+                    System.out.println("No se pudo dar de baja al empleado.");
+                }
+
+                psActualizarEstado.close();
+            } else {
+                System.out.println("No se encontraron empleados en la base de datos.");
+            }
+
+            resultadoEmpleado.close();
+            psEmpleado.close();
+            } catch (ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Error al cargar al driver");
+        } catch (SQLException ex){
+           int error= ex.getErrorCode();
+           if(error==1146){
+               JOptionPane.showMessageDialog(null, "Tabla Inexistente");
+           } else if(error==1049){
+               JOptionPane.showMessageDialog(null, "BD inexistente");
+           } else{
+               JOptionPane.showMessageDialog(null, "Error SQL");
+           }
+        }*/
+    }
+}
